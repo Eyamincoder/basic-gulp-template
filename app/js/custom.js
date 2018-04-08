99b98a2577feeb19f94b2f2820177e0f2480d5c6
Eyamin Hossain <eyamin.coder@gmail.com>
2018-04-08 21:20:59 +0600

    Initial Commit

--- app/js/custom.js
+++
@@ -1,6 +0,0 @@
-// This line for js compression test
-console.log('Gulp js comression using uglify');
-
-document.getElementById("myButton").onclick = function () {
-     location.href = "https://www.facebook.com/eyamin28";
- };
--- app/js/custom2.js
+++
@@ -1,6 +0,0 @@
-// This line for js compression test
-console.log('Gulp js comression using uglify hello how are you');
-
-document.getElementById("myButton").onclick = function () {
-     location.href = "https://www.facebook.com/eyamin28";
- };
